417df43cb9ad134b4682a678a05c2ff3bf88e56c
Rich Samartino <rich@richsamartino.com>
2019-10-03 15:16:24 -0700

    improve grid controller
    require customer to be logged in to view; add page title

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -2,26 +2,53 @@
 
 namespace Rich\LoginHistory\Controller\Index;
 
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\Result\PageFactory;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+    /**
+     * @var Session
+     */
+    protected $customerSession;
+
     /**
      * @var PageFactory
      */
     protected $resultPageFactory;
 
     /**
+     * Index constructor.
      * @param Context $context
+     * @param Session $customerSession
      * @param PageFactory $resultPageFactory
      */
     public function __construct(
         Context $context,
+        Session $customerSession,
         PageFactory $resultPageFactory
+
     ) {
         parent::__construct($context);
+        $this->customerSession = $customerSession;
         $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Check customer authentication to show page
+     *
+     * @param RequestInterface $request
+     * @return \Magento\Framework\App\ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function dispatch(RequestInterface $request)
+    {
+        if (!$this->customerSession->authenticate()) {
+            $this->_actionFlag->set('', self::FLAG_NO_DISPATCH, true);
+        }
+        return parent::dispatch($request);
     }
 
     /**
@@ -29,6 +56,10 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        return $this->resultPageFactory->create();
+        $resultPage = $this->resultPageFactory->create();
+
+        $resultPage->getConfig()->getTitle()->set(__('My Login History'));
+
+        return $resultPage;
     }
 }
