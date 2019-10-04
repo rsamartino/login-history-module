4f6555c65d267894328e512fa98f7189f8faf235
Rich Samartino <rich@richsamartino.com>
2019-10-03 20:20:16 -0700

    add csv export button to frontend grid

---
+++ Controller/Export/GridToCsv.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Rich\LoginHistory\Controller\Export;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\Response\Http\FileFactory;
+use Magento\Ui\Component\MassAction\Filter;
+use Magento\Ui\Model\Export\ConvertToCsv;
+use Psr\Log\LoggerInterface;
+
+class GridToCsv extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var ConvertToCsv
+     */
+    protected $converter;
+
+    /**
+     * @var FileFactory
+     */
+    protected $fileFactory;
+
+    /**
+     * @var Filter
+     */
+    private $filter;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param Context $context
+     * @param ConvertToCsv $converter
+     * @param FileFactory $fileFactory
+     * @param Filter|null $filter
+     * @param LoggerInterface|null $logger
+     */
+    public function __construct(
+        Context $context,
+        ConvertToCsv $converter,
+        FileFactory $fileFactory,
+        Filter $filter = null,
+        LoggerInterface $logger = null
+    ) {
+        parent::__construct($context);
+        $this->converter = $converter;
+        $this->fileFactory = $fileFactory;
+        $this->filter = $filter ?: ObjectManager::getInstance()->get(Filter::class);
+        $this->logger = $logger ?: ObjectManager::getInstance()->get(LoggerInterface::class);
+    }
+
+    /**
+     * Export data provider to CSV
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @return \Magento\Framework\App\ResponseInterface
+     */
+    public function execute()
+    {
+        return $this->fileFactory->create('export.csv', $this->converter->getCsvFile(), 'var');
+    }
+}
